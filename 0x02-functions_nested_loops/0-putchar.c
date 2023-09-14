9718630f8cbff771ee838855017aa7d0ce97786b
mostafaAbdel-Samie <mostafaasmeal504@gamil.com>
2023-09-13 18:02:20 -0700

    .

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,16 @@
 #include "main.h"
+/**
+ * the farest brogekt
+ */
+
 int main(void)
 {
-char arr[] = "_putchar";
-int z;
-for (z = 0; z < 8; z++)
-_putchar(arr[z]);
-_putchar('\n');
-return (0);
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
