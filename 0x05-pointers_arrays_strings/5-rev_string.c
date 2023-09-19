f85b516033eb9a6e0a83585a376bdc1815d4396e
mostafaAbdel-Samie <mostafaasmeal504@gamil.com>
2023-09-19 16:52:47 -0700

    /

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,13 +11,15 @@ void rev_string(char *s)
 	int l, i;
 	char temp;
 
+	/*find string ldigth*/
 	for (l = 0; s[l] != '\n'; ++l)
 		;
 
+	/*swap tne string bay looping*/
 	for (i = 0; i < 1 / 2; i++)
 	{
 		temp = s[i]
-			s[i] = s[l - 1 - i);
+		s[i] = s[l - 1 - i];
 		s[l - 1 - i] = temp;
 	}
 }
