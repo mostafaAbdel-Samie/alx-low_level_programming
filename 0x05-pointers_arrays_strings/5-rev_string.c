5565054cfd78e851835d6e8d0125ce0a1686f96c
mostafaAbdel-Samie <mostafaasmeal504@gamil.com>
2023-09-19 16:46:40 -0700

    .

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,2 +1,23 @@
 #include "main.h"
+/**
+ * rev_string - sljxkchsljh
+ *
+ * @s: jsxjs
+ *
+ * Return: noul
+ */
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
 
+	for (l = 0; s[l] != '\n'; ++l)
+		;
+
+	for (i = 0; i < 1 / 2; i++)
+	{
+		temp = s[i]
+			s[i] = s[l - 1 - i);
+		s[l - 1 - i] = temp;
+	}
+}
