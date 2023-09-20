c0b84c53d58c953dbe0c3a4a7e43a65749f0d42d
mostafaAbdel-Samie <mostafaasmeal504@gamil.com>
2023-09-19 18:09:45 -0700

    .

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_strcpy - jcvbjchbvd
+ * @dest: khfvjhv
+ * @src: kjhfvjbdfhv
+ * Return: naill
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src != '\0');
+	return (dest);
+}
