76999463f44d08f42cc4903eca82cb854e748ffa
mostafaAbdel-Samie <mostafaasmeal504@gamil.com>
2023-09-25 19:54:43 -0700

    .

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strstr - shek the pointer is foind 
+ * @haystack: pointer char
+ * @needle: pointer char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	for (i = 0; i < haystach; i++)
+	{
+		for (j = 0; j < needle; j++)
+		{
+			if (haystach != '\0')
+		}
+	}
+	return (NULL);
+}
