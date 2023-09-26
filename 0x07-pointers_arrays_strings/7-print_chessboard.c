7f42c850c711b3aa1bd280dfad0de524e5bc0780
mostafaAbdel-Samie <mostafaasmeal504@gamil.com>
2023-09-25 19:33:11 -0700

    /

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,11 +6,11 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i;
+	int i, j;
 
 	for (i = 0 i < 8; i++)
 	{
-		for (int j = 0; j < 8; j++)
+		for ( j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
